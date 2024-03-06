8cd0b2f9140a2b352999da1db7dfe09cfa250156
Masao Ishihara <masaoishihara@wsazqaz.local>
2024-03-07 02:33:51 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+
+def to_morse_code(word: str):
+    word = word.upper()
+    morse_code_list = []
+    for character in word:
+        if character in MORSE_CODE_DICT:
+            morse_code_list.append(MORSE_CODE_DICT[character])
+        else:
+            morse_code_list.append('?')
+    return "".join(morse_code_list)
+
+
+user_input = input("Enter a string: ")
+print(to_morse_code(user_input))
